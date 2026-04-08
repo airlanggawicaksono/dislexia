152bc9c9574c436ff1c37ff605cf0bc2ea221972
wicaksonoleksono <wcksnlxn@gmail.com>
2026-04-08 10:18:52 +0700

    boiler

--- dyslexia/lib/src/app.dart
+++ dyslexia/lib/src/app.dart
@@ -5,11 +5,9 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'configs/injector/injector_conf.dart';
 import 'core/blocs/theme/theme_bloc.dart';
-import 'core/blocs/translate/translate_bloc.dart';
 import 'core/themes/app_theme.dart';
-import 'features/auth/presentation/bloc/auth/auth_bloc.dart';
+import 'features/display_settings/presentation/bloc/display_settings/display_settings_bloc.dart';
 import 'routes/app_route_conf.dart';
-import 'routes/app_route_path.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -30,43 +28,20 @@ class MyApp extends StatelessWidget {
               create: (_) => getIt<ThemeBloc>(),
             ),
             BlocProvider(
-              create: (_) => getIt<TranslateBloc>(),
-            ),
-            BlocProvider(
-              create: (_) =>
-                  getIt<AuthBloc>()..add(AuthCheckSignInStatusEvent()),
+              create: (_) => getIt<DisplaySettingsBloc>(),
             ),
           ],
-          child: BlocListener<AuthBloc, AuthState>(
-            listenWhen: (_, current) =>
-                current is AuthCheckSignInStatusSuccessState,
-            listener: (_, state) {
-              if (state is AuthCheckSignInStatusSuccessState) {
-                final user = state.data;
-                final userMap = {
-                  "user_id": user.userId ?? "",
-                  "email": user.email ?? "",
-                  "username": user.username ?? "",
-                };
-
-                router.goNamed(
-                  AppRoute.home.name,
-                  pathParameters: userMap,
-                );
-              }
+          child: BlocBuilder<ThemeBloc, ThemeState>(
+            builder: (_, state) {
+              return MaterialApp.router(
+                debugShowCheckedModeBanner: false,
+                localizationsDelegates: context.localizationDelegates,
+                supportedLocales: context.supportedLocales,
+                locale: context.locale,
+                theme: AppTheme.data(state.isDarkMode),
+                routerConfig: router,
+              );
             },
-            child: BlocBuilder<ThemeBloc, ThemeState>(
-              builder: (_, state) {
-                return MaterialApp.router(
-                  debugShowCheckedModeBanner: false,
-                  localizationsDelegates: context.localizationDelegates,
-                  supportedLocales: context.supportedLocales,
-                  locale: context.locale,
-                  theme: AppTheme.data(state.isDarkMode),
-                  routerConfig: router,
-                );
-              },
-            ),
           ),
         ),
       ),
